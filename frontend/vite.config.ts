f25f25ea84a544a817dc6d51b76559466b4856c3
Shivam_Gawade <24ec25@aitdgoa.edu.in>
2026-08-22 09:56:25 +0000

    chore: configure Vercel frontend deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "../backend/app/frontend",
+    outDir: process.env.VERCEL ? "dist" : "../backend/app/frontend",
     emptyOutDir: true,
   },
   resolve: {
